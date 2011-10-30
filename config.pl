1880e2bbc814c78c92097b17ac17937fce401ab0
Masayuki Matsuki <y.songmu@gmail.com>
2011-10-30 23:26:55 +0900

    add database config

--- config.pl
+++ config.pl
@@ -1,3 +1,11 @@
-+{
+my $home = Jobeet::Models->get('home');
+
+return {
     default_view    => 'MT',
-}
+    database => [
+        'dbi:SQLite:' . $home->file('database.db'), '', '',
+         {
+             sqlite_unicode => 1,
+         },
+    ],
+};
