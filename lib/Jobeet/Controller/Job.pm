955f3d0aa448279de1bc5dd440f12313a408c4f1
Masayuki Matsuki <y.songmu@gmail.com>
2011-10-31 18:50:14 +0900

    add actions to Job

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -9,5 +9,29 @@ sub index :Path {
     $c->stash->{jobs} = models('Schema::Job');
 }
 
-1;
+# /job/{job_id} （詳細）
+sub show :Path :Args(1) {
+    my ($self, $c, $job_id) = @_;
+}
 
+# /job/create （新規作成）
+sub create :Local {
+    my ($self, $c) = @_;
+}
+
+sub job :Chained('/') :PathPart :CaptureArgs(1) {
+    my ($self, $c, $job_id) = @_;
+    $c->stash->{job_id} = $job_id;
+}
+
+# /job/{job_id}/edit （編集）
+sub edit :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+# /job/{job_id}/delete （削除）
+sub delete :Chained('job') :PathPart :Args(0) {
+    my ($self, $c) = @_;
+}
+
+1;
