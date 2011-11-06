f6b350a279c5ff7c99c58396a54776bbc8d9da1d
Masayuki Matsuki <y.songmu@gmail.com>
2011-11-06 14:23:16 +0900

    refactor Category#get_active_jobs

--- lib/Jobeet/Schema/Result/Category.pm
+++ lib/Jobeet/Schema/Result/Category.pm
@@ -47,14 +47,12 @@ sub get_active_jobs {
     my $self = shift;
     my $attr = shift || {};
 
-    $attr->{rows} ||= 10;
-
-    $self->jobs({
-        expires_at  => { '>=', models('Schema')->now }
-    }, {
-        order_by    => { -desc => 'created_at' },
-        rows        => $attr->{rows},
-    });
+    $self->jobs(
+        { expires_at => { '>=', models('Schema')->now } },
+        {   order_by => { -desc => 'created_at' },
+            defined $attr->{rows} ? (rows => $attr->{rows}) : (),
+        }
+    );
 }
 
 sub insert {
