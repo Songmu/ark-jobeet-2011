23df0c0a8bb9aacbe4af627b1c99e8bf98477a2f
Masayuki Matsuki <y.songmu@gmail.com>
2011-11-07 20:24:47 +0900

    Job actions

--- lib/Jobeet/Controller/Job.pm
+++ lib/Jobeet/Controller/Job.pm
@@ -31,18 +31,38 @@ sub create :Local :Form('Jobeet::Form::Job') {
 }
 
 sub job :Chained('/') :PathPart :CaptureArgs(1) {
-    my ($self, $c, $job_id) = @_;
-    $c->stash->{job_id} = $job_id;
+    my ($self, $c, $job_token) = @_;
+
+    $c->stash->{job} = models('Schema::Job')->find({ token => $job_token })
+        or $c->detach('/default');
 }
 
 # /job/{job_id}/edit （編集）
-sub edit :Chained('job') :PathPart :Args(0) {
+sub edit :Chained('job') :PathPart :Form('Jobeet::Form::Job') {
     my ($self, $c) = @_;
+
+    my $job = $c->stash->{job};
+
+    if ($c->req->method eq 'POST') {
+        if ($self->form->submitted_and_valid) {
+            $job->update_from_form($self->form);
+            $c->redirect( $c->uri_for('/job', $job->token) );
+        }
+    }
+    else {
+        $self->form->fill({
+            $job->get_columns,
+            category => $job->category->name,
+        });
+    }
 }
 
 # /job/{job_id}/delete （削除）
-sub delete :Chained('job') :PathPart :Args(0) {
+sub delete :Chained('job') :PathPart {
     my ($self, $c) = @_;
+
+    $c->stash->{job}->delete;
+    $c->redirect( $c->uri_for('/job') );
 }
 
 1;
